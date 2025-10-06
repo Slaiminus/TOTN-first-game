eaa98b52cdef769305c963658466ea2f0b39f2ee
sakulopa 360 <7507058@gmail.com>
2025-10-06 11:01:26 +0300

    починил скрирпт ротации руки
    скрирпт ротации руки работает вне зависимости от позиции камеры

--- Assets/Scripts/arm_rotate.cs
+++ Assets/Scripts/arm_rotate.cs
@@ -4,10 +4,13 @@ using System.Collections;
 
 public class arm_rotate : MonoBehaviour
 {
+    [SerializeField] private float camoff_y;
+    [SerializeField] private float camoff_x;
+
     void Update()
     {
         Vector3 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = (180 / Mathf.PI) * Mathf.Atan2(mousePos.y - Camera.main.transform.position.y, mousePos.x - Camera.main.transform.position.x);
+        float angle = (180 / Mathf.PI) * Mathf.Atan2(mousePos.y - (Camera.main.transform.position.y - camoff_y), mousePos.x - (Camera.main.transform.position.x - camoff_x));
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
